285b3ee5b0516966a665e35c452a3d66b9d98a9a
michealcoleyoung <coleyoungmusic@gmail.com>
2025-07-27 16:11:36 -0500

    added mybash function with absolute path

--- myfunctions.sh
+++ myfunctions.sh
@@ -3,12 +3,18 @@
 myfunctions() {
   cat << EOF
 ========== List of Available Commands ==========
+mybash: Opens up myfunctions.sh file in Neovim
 kden: Opens kdenlive with a scale factor of 2 
 simple: Opens simplescreenrecorder with a scale factor of 2
 pyenv: Creates python virtual environment
 activate_venv: Activates python virtual environment
 update: Makes necessary updates to machine
 EOF
+}
+
+# Opens up myfunctions.sh script in Neovim
+mybash() {
+  nvim "$HOME/.config/bash/mybash/myfunctions.sh"
 }
 
 # Sets kdenlive to a scale factor of 2 when opened
@@ -30,6 +36,7 @@ activate_venv() {
   source venv/bin/activate
 }
 
+# Updates system
 update() {
   sudo apt update && sudo apt upgrade -y
   sudo apt autoremove -y && sudo apt clean
